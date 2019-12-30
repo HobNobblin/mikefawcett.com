2708e34424f4ea05aa5fc4ba789025204a5777a7
Mike Fawcett <mike.fawcett@aig.com>
2019-12-30 13:00:05 -0500

    added automatic age calculation for about page.  updated resume

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+    selector: 'app-about',
+    templateUrl: './about.component.html',
+    styleUrls: ['./about.component.scss']
 })
+
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+    ageYear: Number = 1978;
+    ageMonth: Number = 6;
+    ageDay: Number = 2;
+    age: Number = 0;
 
-  ngOnInit() {
-  }
+    constructor() { }
+
+    ngOnInit() {
+        let year: Number = (new Date()).getFullYear();
+        let month: Number = (new Date()).getMonth() + 1;
+        let day: Number = (new Date()).getDate();
+
+        if( month > 7 || (month == 6 && day > 2) ) {
+            this.age = year - 1978;
+        } else {
+            this.age = year - 1979;
+        }
+    }
 
 }
